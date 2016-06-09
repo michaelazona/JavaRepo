71bf4ff3ee33d38448cd5092435ffbeb8e655ae6
zonam1 <zonam1@ZONAM1-L1.ent.core.medtronic.com>
2016-06-09 10:53:05 -0500

    Here's the rest.

---
+++ JUnitProject/src/Code/Customer.java
@@ -0,0 +1,33 @@
+package Code;
+
+public class Customer 
+{
+	String firstName = null;
+	String lastName  = null;
+	
+	public String getCustomerName()
+	{
+		String returnValue = null;
+		
+		if((this.firstName != null) && (this.lastName != null))
+		{
+			returnValue = this.firstName + " " + this.lastName;
+		}
+		
+		else if(this.firstName != null){ returnValue = this.firstName + " N/A";	}
+		else if(this.lastName != null) { returnValue =  "N/A " + this.lastName;	}
+		else { returnValue = "N/A N/A"; }
+		
+		return returnValue;
+	}
+	
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+}
